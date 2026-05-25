50cb975374202179b0ba83f3daac4ee8731afc8e
Ilya <pankratovilyasergeevich@gmail.com>
2026-05-26 02:19:51 +0300

    Update to latest version.

--- Offset_Sortr.py
+++ Offset_Sortr.py
@@ -3,8 +3,11 @@ import re
 import shutil
 import time
 import funcs
+import json
 from PyPDF2 import PdfReader
-from config import directories
+from PyPDF2.errors import PdfReadError
+
+# from config import data
 
 COLOR_4_0 = ['1+0', '4+0']
 COLOR_4_4 = ['1+1', '4+4']
@@ -17,6 +20,18 @@ def product_size_to_mm(product_size: str) -> list[int]:
     product_size = ''.join('x' if char.isalpha() else char for char in product_size)
 
     return sorted([int(size) for size in product_size.split('x')])
+
+
+with open('config.json', 'r', encoding='utf-8-sig') as file:
+    data = json.load(file)
+
+    # Получить все имена не стандартных папкок и путей к ним из файлика конфигурации .ру
+    # 1) Имена не стандартных папок (а точнее их размеры)
+    all_podpis_sizes = [product_size_to_mm(file['podpis']) for file in data['special files']]
+
+    # 2) Пути к стандартным папкам
+    error = data['error files']
+    other = data['other files']
 
 
 def replacer(filename: str, destination: str) -> None:
@@ -73,7 +88,7 @@ def CropBox_equal_special_product_size(file: PdfReader, product_size: str) -> bo
     Размер готового изделия указан в подписи .pdf документа. """
 
     special_file_size = 0
-    for special_file in directories['special files']:
+    for special_file in data['special files']:
         if product_size_to_mm(special_file['podpis']) == product_size_to_mm(product_size):
             special_file_size = special_file['bleeds']
 
@@ -91,14 +106,6 @@ def CropBox_equal_special_product_size(file: PdfReader, product_size: str) -> bo
         return False
 
 
-# Получить все имена не стандартных папкок и путей к ним из файлика конфигурации .ру
-# 1) Имена не стандартных папок (а точнее их] размеры)
-all_podpis_sizes = [product_size_to_mm(file['podpis']) for file in directories.get('special files')]
-
-# 2) Пути к стандартным папкам
-error = directories['error files']
-other = directories['other files']
-
 # Из указанной директории проанализировать все файлы
 input_dir = input('Select folder:\n')
 
@@ -109,13 +116,58 @@ while True:
 
         for filename in funcs.get_all_filenames_in_directory(input_dir):
 
+            if filename[-8:] == '_lic.pdf' in filename:
+                result_name = filename.replace(filename[-8:], '.pdf')
+
+            elif filename[-7:] == '_ob.pdf' in filename:
+                result_name = filename.replace(filename[-7:], '.pdf')
+
+            else:
+                result_name = filename
+
+            # Сначала выполнить все проверки ПДФ файла, затем направить его в соответствующую папку.
+
             product_size, color = get_params_from_filename(filename)
             product_size_mm = product_size_to_mm(product_size)
 
-            pdf_file = PdfReader(filename)
-            pages = len(pdf_file.pages)
+            FILE = False
+            DIR = False
 
-            if product_size_mm not in all_podpis_sizes:
+            if os.path.isfile(filename):
+                FILE = True
+                pdf_file = PdfReader(filename)
+                pages = len(pdf_file.pages)
+
+            elif os.path.isdir(filename):
+                DIR = True
+                files_in_directory = funcs.get_all_filenames_in_directory(filename)
+                # files_in_directory = funcs.get_all_filenames_in_directory(f"{from_path}\\{filename}")
+
+                # Проверка количества файлов внутри папки.
+                if len(files_in_directory) != 2:
+                    print(f'''[{funcs.get_current_time()}]   {filename}'
+                    \rКол-во файлов внутри папки не равно двум. Направляю в ошибки.\n''')
+                    replacer(filename, os.path.join(error, filename))
+                    continue
+
+                # Путь до конкретного файла внутри директории - os.path.join(from_path, filename, file)
+                lic, ob = (PdfReader(os.path.join(input_dir, filename, file)) for file in files_in_directory)
+
+                # Проверка количества страниц документа внутри папки.
+                if len(lic.pages) != 1 or len(ob.pages) != 1:
+                    print(f'''[{funcs.get_current_time()}]   {filename}'
+                    \rФайл внутри папки содержит более одной страницы. Направляю в ошибки.\n''')
+                    replacer(filename, os.path.join(error, filename))
+                    continue
+
+            else:
+                print(f'''[{funcs.get_current_time()}]   {filename}'
+                      \rОбъект не является ни файлом ни папкой, направляю его в папку с ошибками.\n''')
+                replacer(filename, os.path.join(error, filename))
+                continue
+
+            # Проверка для файлов ==================================================================================
+            if FILE and product_size_mm not in all_podpis_sizes:
 
                 if not check_colorify(color, pages):
                     print(f'[{funcs.get_current_time()}]   {filename}\nЦветность документа не соответствует подписи.\n')
@@ -133,9 +185,10 @@ while True:
                     replacer(filename, os.path.join(error, filename))
 
                 else:
-                    replacer(filename, os.path.join(other, filename))
+                    replacer(filename, os.path.join(other, result_name))
 
-            else:
+            elif FILE and product_size_mm in all_podpis_sizes:
+
                 if not check_colorify(color, pages):
                     print(f'[{funcs.get_current_time()}]   {filename}\nЦветность документа не соответствует подписи.\n')
                     replacer(filename, os.path.join(error, filename))
@@ -153,15 +206,94 @@ while True:
 
                 else:
                     destination = ''
-                    for files in directories['special files']:
+                    for files in data['special files']:
+                        if product_size_to_mm(files['podpis']) == product_size_mm:
+                            destination = files['folder']
+
+                    replacer(filename, os.path.join(destination, result_name))
+
+
+            # Проверка для ПАПОК ==================================================================================
+            elif DIR and product_size_mm not in all_podpis_sizes:
+
+                if color in COLOR_4_0:
+
+                    print(f'''[{funcs.get_current_time()}]   {filename}
+                    \rПапка Должна иметь двухстороннюю печать. Сейчас её подпись {color}. Направляю в ошибки.\n''')
+                    replacer(filename, os.path.join(error, filename))
+
+                elif not (
+                        funcs.CropBox_equal_product_size(lic, product_size) and
+                        funcs.CropBox_equal_product_size(ob, product_size)):
+
+                    print(f'''[{funcs.get_current_time()}]   {filename}
+                    \rCropBox документа внутри папки не соответствуетразмеру подписи {product_size}.\n''')
+                    replacer(filename, os.path.join(error, filename))
+
+                elif not (
+                        funcs.all_pages_are_landscape(lic, product_size) and
+                        funcs.all_pages_are_landscape(ob, product_size)
+                        or
+                        funcs.all_pages_are_portrait(lic, product_size) and
+                        funcs.all_pages_are_portrait(ob, product_size)):
+
+                    print(f'''[{funcs.get_current_time()}]   {filename}
+                    \rСтраницы документа внутри папки имеют разную ориентацию..\n''')
+                    replacer(filename, os.path.join(error, filename))
+
+                else:
+                    replacer(filename, os.path.join(other, filename))
+
+
+            elif DIR and product_size_mm in all_podpis_sizes:
+
+                if color in COLOR_4_0:
+
+                    print(f'''[{funcs.get_current_time()}]   {filename}
+                    \rПапка Должна иметь двухстороннюю печать. Сейчас её подпись {color}. Направляю в ошибки.\n''')
+                    replacer(filename, os.path.join(error, filename))
+
+                elif not (
+                        CropBox_equal_special_product_size(lic, product_size) and
+                        CropBox_equal_special_product_size(ob, product_size)):
+
+                    print(f'''[{funcs.get_current_time()}]   {filename}
+                    \rCropBox документа внутри папки не соответствуетразмеру подписи {product_size}.\n''')
+                    replacer(filename, os.path.join(error, filename))
+
+                elif not (
+                        funcs.all_pages_are_landscape(lic, product_size) and
+                        funcs.all_pages_are_landscape(ob, product_size)
+                        or
+                        funcs.all_pages_are_portrait(lic, product_size) and
+                        funcs.all_pages_are_portrait(ob, product_size)):
+
+                    print(f'''[{funcs.get_current_time()}]   {filename}
+                    \rСтраницы документа внутри папки имеют разную ориентацию..\n''')
+                    replacer(filename, os.path.join(error, filename))
+
+                else:
+                    destination = ''
+                    for files in data['special files']:
                         if product_size_to_mm(files['podpis']) == product_size_mm:
                             destination = files['folder']
 
                     replacer(filename, os.path.join(destination, filename))
 
-                # Сначала выполнить все проверки ПДФ файла, затем направить его в соответствующую папку.
+
+    except IndexError:
+        print(f'[{funcs.get_current_time()}]   Не понимаю имя файла {filename}.\nНаправляю его в папку с ошибками.\n')
+        replacer(filename, os.path.join(error, filename))
+
+    except PdfReadError:
+        # EmptyFileError
+        print(f'''[{funcs.get_current_time()}]   {filename}
+        \rОшибка при чтении .pdf - файлика. Направляю в ошибки.\n''')
+        replacer(filename, os.path.join(error, filename))
 
     except Exception as E:
         print(E)
+        print(f'[{funcs.get_current_time()}]   Произошла неожиданная ошибка. Повторяю попытку.')
+
     finally:
         time.sleep(3)
---
+++ funcs.py
@@ -0,0 +1,347 @@
+﻿import os
+import re
+import shutil
+import time
+from PyPDF2 import PdfReader, PageObject
+from decimal import Decimal
+from typing import Callable, Tuple, Any
+
+COLOR_4_0 = ['1+0', '4+0', '5+0']
+COLOR_4_4 = ['1+1', '4+4', '5+5']
+
+SRA3 = {
+    'name': 'SRA3',
+    'file_signature': ['450x320', '320x450'],
+    'size_value': [320, 450],
+    'height': 320,
+    'width': 450
+}
+
+SRA3_PLUS = {
+    'name': 'SRA3+',
+    'file_signature': ['487x330', '330x487'],
+    'size_value': [330, 487],
+    'height': 330,
+    'width': 487
+}
+
+PAGE_SIZE_DIAPASON = 10
+VILETI = 4
+
+ALLOWED_VIZ_SIZES = {
+    'file_signature': ['89x49', '49x89', '90x50', '50x90'],
+    'size_value': [[53, 93], [54, 94]],
+}
+
+
+def get_current_time():
+    return time.strftime("%H:%M:%S")
+
+
+def exit_program(time_sleep: int, exit_code: int) -> None:
+    """ Прекращение исполнения программы через заданное время. """
+
+    time.sleep(time_sleep)
+    exit(exit_code)
+
+
+def get_all_filenames_in_directory(path: str) -> list[str]:
+    """ Читает имена всех файлов и папок в указанной директории. """
+
+    try:
+        return [file.name for file in os.scandir(path)]
+    except FileNotFoundError:
+        print('Не верный адрес.')
+        exit_program(5, 1)
+
+
+def get_params_from_filename(filename: str) -> list[str | int] | bool:
+    """ Возвращает список параметров .pdf файла из его названия.
+     Пример имени файла:
+     12-05_1108608_2v_160_120x45_4+0_350mat_lamGL1+1_2SRA3_po_160_listov.pdf """
+
+    multy_page_products = r'(?P<product>spring|brushura|catalog|bloknot)'
+    right_filename_pattern = r'(?i).*?' \
+                             r'(?P<f_product_size>\d+[xх]\d+).*?' \
+                             r'(?P<color>\d\+\d).*?' \
+                             r'(?P<quantity>\d*)(?:[ -]|)' \
+                             r'(?P<print_sheet_size>SRA\d\+?).*?' \
+                             r'(?P<extra>--)?' \
+                             r'(?:$|\.)'
+
+    result = re.findall(right_filename_pattern, filename)
+
+    if result and not re.search(multy_page_products, filename):
+        f_product_size, f_colorify, f_quantity, f_print_sheet_size, extra = result[0]
+        f_quantity = int(f_quantity) if f_quantity.isdigit() else 1
+
+        return [f_product_size, f_colorify, f_quantity, f_print_sheet_size, extra]
+
+    return False
+
+
+def get_multy_page_params_from_filename(filename: str) -> list[str | int] | bool:
+    """ Возвращает список параметров многостранчиного (брошюры на скобу и пружину, каталоги) .pdf файла из его названия.
+     Пример имени файла:
+     01_31(01)_1137493_brushura_OBL_4+0_200x300_4str_250mat_lamMAT1+0_1_scoba_SRA3_1_list.pdf """
+
+    color_pattern = r'_\d\+\d_'
+    multi_pages_pattern = r'(?i).*?' \
+                          r'(?P<product>spring|brushura|catalog|bloknot).*?' \
+                          r'(?P<quantity>\d*)(?:[ -]|)' \
+                          r'(?P<print_sheet_size>SRA\d\+?)'
+
+    result = re.findall(multi_pages_pattern, filename)
+
+    if result:
+        product, quantity, print_sheet_size = result[0]
+        quantity = int(quantity) if quantity.isdigit() else 1
+
+        if re.search(color_pattern, filename):
+            colorify = re.search(color_pattern, filename)[0]
+            colorify = colorify.replace('_', '')
+        else:
+            colorify = '4+4'
+
+        return [product, colorify, quantity, print_sheet_size]
+
+    return False
+
+
+def replacer(filename: str, destination: str) -> None:
+    """ Перемещает файл в указанную директорию. Всего 3 возможных варианта:
+    1) Перемещение файла в указанную директорию
+    2) Перезапись папки и содержимого
+    3) Перемещение папки в указанную директорию
+    """
+
+    if os.path.isfile(filename):
+        os.replace(filename, destination)
+
+    elif os.path.isdir(filename) and os.path.exists(destination):
+        shutil.rmtree(destination)
+        os.replace(filename, destination)
+
+    elif os.path.isdir(filename):
+        os.replace(filename, destination)
+
+    else:
+        raise NotADirectoryError('replacer function Error!')
+
+
+def check_colorify(f_colorify: str, f_quantity: int, pages: int) -> bool:
+    """ Проверка цветности документа, так же учитываются виды. """
+
+    if f_colorify in COLOR_4_4 and pages == f_quantity * 2 or \
+            f_colorify in COLOR_4_0 and pages == f_quantity:
+        return True
+
+    return False
+
+
+def decimal_to_mm(size: Decimal) -> int:
+    """ Преобразуем inch в целочисленное значение метрической системы. """
+
+    return int(size // Decimal(2.83))
+
+
+def product_size_to_mm(product_size: str) -> list[int]:
+    """ Возвращает список целочисленных значений ВхШ страницы документа. Результат отсортирован по возрастанию. """
+
+    product_size = ''.join('x' if char.isalpha() else char for char in product_size)
+
+    return sorted([int(size) for size in product_size.split('x')])
+
+
+def all_pages_has_same_size(file: PdfReader) -> bool:
+    """ Отвечает на вопрос равенства ВхШ всех страниц документа без учёта поворота. """
+
+    if not isinstance(file, PdfReader):
+        return False
+
+    all_pages_sizes = list()
+    for page in file.pages:
+        page_size = [decimal_to_mm(page.cropbox.height), decimal_to_mm(page.cropbox.width)]
+        page_size.sort()
+        all_pages_sizes.append(page_size)
+
+    return all(map(lambda x: x == all_pages_sizes[0], all_pages_sizes))
+
+
+def all_pages_has_same_size_checker(func: Callable) -> Callable:
+    """ Декоратор. Предварительная проверка документа, на равенство размеров всех страниц между собой. """
+
+    def wrapper(file: PdfReader, file_size: str) -> bool:
+        return all_pages_has_same_size(file) and func(file, file_size)
+
+    return wrapper
+
+
+def get_current_page_size(page: PageObject) -> list[int]:
+    """ Возвращает список из фактической (видимой) высоты и ширины текущей страницы документа. """
+
+    return [decimal_to_mm(page.cropbox.height), decimal_to_mm(page.cropbox.width)]
+
+
+def get_product_sides(product_size: str) -> Tuple[int | Any, int | Any]:
+    """ Возвращает размеры сторон изделия, сперва меньшую сторону, затем большую сторону. """
+
+    if product_size in ALLOWED_VIZ_SIZES['file_signature']:
+        small_product_side, big_product_side = 50, 90
+
+    elif product_size == SRA3['name']:
+        small_product_side = SRA3['height']
+        big_product_side = SRA3['width']
+
+    elif product_size == SRA3_PLUS['name']:
+        small_product_side = SRA3_PLUS['height']
+        big_product_side = SRA3_PLUS['width']
+
+    else:
+        small_product_side, big_product_side = product_size_to_mm(product_size)
+
+    return small_product_side, big_product_side
+
+
+def all_pages_are_landscape(file: PdfReader, product_size: str) -> bool:
+    """ Выполняет проверку на горизонтальное положение всех страниц документа. """
+
+    small_product_side, big_product_side = product_size_to_mm(product_size)
+
+    if small_product_side == big_product_side:
+        return True
+
+    for page in file.pages:
+        file_height, file_width = get_current_page_size(page)
+
+        if page.rotation % 180 == 0 and \
+                file_height > big_product_side:
+            return False
+
+        if page.rotation % 90 == 0 and page.rotation % 180 != 0 and \
+                file_width > big_product_side:
+            return False
+
+    return True
+
+
+def all_pages_are_portrait(file: PdfReader, product_size: str) -> bool:
+    """ Выполняет проверку на вертикальное положение всех страниц документа. """
+
+    small_product_side, big_product_side = product_size_to_mm(product_size)
+
+    if small_product_side == big_product_side:
+        return True
+
+    for page in file.pages:
+        file_height, file_width = get_current_page_size(page)
+
+        if page.rotation % 180 == 0 and \
+                file_width > big_product_side:
+            return False
+
+        if page.rotation % 90 == 0 and page.rotation % 180 != 0 and \
+                file_height > big_product_side:
+            return False
+
+    return True
+
+
+@all_pages_has_same_size_checker
+def CropBox_equal_product_size(file: PdfReader, product_size: str) -> bool:
+    """ Фактический (видимый) размер документа равен размеру готового изделия.
+    Размер готового изделия указан в подписи .pdf документа. """
+
+    pure_page_size_values = sorted(side_size - VILETI for side_size in get_current_page_size(file.pages[0]))
+    return pure_page_size_values == product_size_to_mm(product_size)
+
+
+@all_pages_has_same_size_checker
+def CropBox_equal_vizitka_90x50_size(file: PdfReader, product_size: str) -> bool:
+    """ Фактический (видимый) размер документа равен размеру визитки (диапазон значений). """
+
+    VIZ_DIAPASON_START, VIZ_DIAPASON_END = ALLOWED_VIZ_SIZES['size_value']
+    VIZ_DIAPASON_START_HEIGHT, VIZ_DIAPASON_START_WIDTH = VIZ_DIAPASON_START
+    VIZ_DIAPASON_END_HEIGHT, VIZ_DIAPASON_END_WIDTH = VIZ_DIAPASON_END
+
+    product_height, product_width = sorted(get_current_page_size(file.pages[0]))
+
+    if product_size in ALLOWED_VIZ_SIZES['file_signature'] and \
+            VIZ_DIAPASON_START_HEIGHT <= product_height <= VIZ_DIAPASON_END_HEIGHT and \
+            VIZ_DIAPASON_START_WIDTH <= product_width <= VIZ_DIAPASON_END_WIDTH:
+        return True
+
+    return False
+
+
+@all_pages_has_same_size_checker
+def CropBox_equal_SRA3_size(file: PdfReader, file_print_sheet_size: str) -> bool:
+    """ Фактический (видимый) размер документа равен SRA3 (320х450 мм). """
+
+    page_height, page_width = sorted(get_current_page_size(file.pages[0]))
+
+    if file_print_sheet_size == SRA3['name'] and \
+            SRA3['height'] - PAGE_SIZE_DIAPASON <= page_height <= SRA3['height'] + VILETI and \
+            SRA3['width'] - PAGE_SIZE_DIAPASON <= page_width <= SRA3['width'] + VILETI:
+        return True
+
+    return False
+
+
+@all_pages_has_same_size_checker
+def CropBox_equal_SRA3_PLUS_size(file: PdfReader, file_print_sheet_size: str) -> bool:
+    """ Фактический (видимый) размер документа равен SRA3+ (330х487 мм). """
+
+    page_height, page_width = sorted(get_current_page_size(file.pages[0]))
+
+    if file_print_sheet_size == SRA3_PLUS['name'] and \
+            SRA3_PLUS['height'] - PAGE_SIZE_DIAPASON <= page_height <= SRA3_PLUS['height'] + VILETI and \
+            SRA3_PLUS['width'] - PAGE_SIZE_DIAPASON <= page_width <= SRA3_PLUS['width'] + VILETI:
+        return True
+
+    return False
+
+
+# ====================================== В папки Hot Folder. ==========================================
+def go_to_SRA3_universal(product_size: str, file_print_sheet_size: str, extra: str) -> bool:
+    """ Отвечает на вопрос, какие файлы идут на раскладку с помощью экшена SRA3_universal. """
+
+    return product_size not in ALLOWED_VIZ_SIZES['file_signature'] and \
+        file_print_sheet_size == SRA3['name'] and \
+        not extra
+
+
+def go_to_SRA3_universal_1_rez(product_size: str, file_print_sheet_size: str, extra: str) -> bool:
+    """ Отвечает на вопрос, какие файлы идут на раскладку с помощью экшена SRA3_universal_1_rez. """
+
+    return product_size not in ALLOWED_VIZ_SIZES['file_signature'] and \
+        file_print_sheet_size == SRA3['name'] and \
+        extra
+
+
+def go_to_SRA3_PLUS_universal(file_print_sheet_size: str, extra: str) -> bool:
+    """ Отвечает на вопрос, какие файлы идут на раскладку с помощью экшена SRA3+_universal. """
+
+    return file_print_sheet_size == SRA3_PLUS['name'] and not extra
+
+
+def go_to_SRA3_PLUS_universal_1_rez(file_print_sheet_size: str, extra: str) -> bool:
+    """ Отвечает на вопрос, какие файлы идут на раскладку с помощью экшена SRA3+_universal_1_rez. """
+
+    return file_print_sheet_size == SRA3_PLUS['name'] and extra
+
+
+def go_to_viz_4_0(product_size: str, file_colorify: str, file_print_sheet_size: str) -> bool:
+    """ Отвечает на вопрос, какие файлы идут на раскладку с помощью экшена viz_4+0. """
+
+    return product_size in ALLOWED_VIZ_SIZES['file_signature'] and \
+        file_colorify in COLOR_4_0 and \
+        file_print_sheet_size == SRA3['name']
+
+
+def go_to_viz_4_4(product_size: str, file_colorify: str, file_print_sheet_size: str) -> bool:
+    """ Отвечает на вопрос, какие файлы идут на раскладку с помощью экшена viz_4+4. """
+
+    return product_size in ALLOWED_VIZ_SIZES['file_signature'] and \
+        file_colorify in COLOR_4_4 and \
+        file_print_sheet_size == SRA3['name']
